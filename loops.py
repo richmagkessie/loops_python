97e8f10edb5915a543597ef2da3b738b21b383a2
Richmond Kessie <kessierich.mag@gmail.com>
2022-10-24 01:33:35 +0000

    Playing with numbers

---
+++ loops.py
@@ -0,0 +1,7 @@
+# Using for loops with ranges
+print('-------------------------')
+print('Number\tSquare')
+#print the square of integers from 1 to 10
+for number in range(1, 11):
+    square = number ** 2
+    print(f'{number}\t{square}')
