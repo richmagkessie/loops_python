c81a9245b67c31a26cf0405fd98b566aee7d0b71
Richmond Kessie <kessierich.mag@gmail.com>
2022-10-24 09:36:16 +0000

    Getting shapes up with turtle

---
+++ designs_in_turtle.py
@@ -0,0 +1,24 @@
+# Using loops to draw Designs
+import turtle
+
+# Defining the screen
+wn = turtle.Screen()
+wn.title('This is turtle trial')
+wn.bgcolor('green')
+wn.setup(width=300, height=500)
+
+# drawing a square shape
+for x in range(4):
+    turtle.forward(100)
+    turtle.right(90)
+
+# drawing an octagon
+for oct in range(8):
+    turtle.forward(100)
+    turtle.right(45)
+
+
+
+# Main loop
+while True:
+    wn.update()
