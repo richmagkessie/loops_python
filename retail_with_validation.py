926ce966b9ed7796517f684197b0e461ccc5ee83
Richmond Kessie <kessierich.mag@gmail.com>
2022-10-24 02:43:45 +0000

    Retail with validation

---
+++ retail_with_validation.py
@@ -0,0 +1,19 @@
+# Christiana owns an import business, she wants to know how much she sell her products at a retail price
+# retail_price = wholesale_price * 2.5
+
+mark_up_percentage = 2.5 # The multiplying factor
+another_item = 'y'        # This would control the loop
+
+# Process one or more items
+while another_item == 'y' or another_item == 'Y':
+    # get the wholesale_price of the item
+    wholesale_price = float(input('Enter the wholesale price of the item: '))
+
+    # Calculate the retail_price
+    retail_price = wholesale_price * mark_up_percentage
+
+    # Display the retail Price
+    print(f'Retail Price: ${retail_price:,.2f}')
+
+    # Do this again?
+    another_item = input('Do you have any more item?' + 'Enter y or Y for yes: ')
